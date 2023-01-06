e60542f9ad41acebe1d6e0b686f1871918990d8d
Luis <luigimonge16@gmail.com>
2023-01-06 19:00:49 +0100

    Define app routes

--- app.py
+++ app.py
@@ -10,31 +10,18 @@ app=Flask(__name__)
 
 #to tell flask what url shoud trigger the function index()
 @app.route('/')
-def index(name):
-    return flask.render_template('index.html', name=name)
+def index():
+    return render_template('index.html')
 
-#prediction function
-def ValuePredictor(to_predict_list):
-    to_predict = np.array(to_predict_list).reshape(1,12)
-    loaded_model = pickle.load(open("finalized_model.pkl","rb"))
-    result = loaded_model.predict(to_predict)
-    return result[0]
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
-@app.route('/result', methods = ['POST'])
-def result():
-    if request.method == 'POST':
-        to_predict_list = request.form.to_dict()
-        to_predict_list=list(to_predict_list.values())
-        to_predict_list = list(map(int, to_predict_list))
-        result = ValuePredictor(to_predict_list)
-        
-        if int(result)==1:
-            prediction='Income more than 50K'
-        else:
-            prediction='Income less that 50K'
-            
-        return render_template("result.html",prediction=prediction)
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
