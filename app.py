7ce0f973854dcdf33a6d0b9dfd0e8ea1fd9e4a23
Luis <luigimonge16@gmail.com>
2023-01-15 13:55:48 +0100

    Add Upload route and further develop create route

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 #importing libraries
 import os
 import numpy as np
-import flask
 import pickle
-from flask import Flask, render_template, request, url_for, flash, redirect
+import os
+# Flask
+from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
+from flask_uploads import UploadSet, IMAGES, configure_uploads
+from flask_wtf import FlaskForm
+from wtforms import SubmitField
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 
+UPLOAD_FOLDER = os.path.join('static', 'uploads')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 # At this point of the project I won't be using a db for two reasons:
 # first, I won't be saving the images that users send as a matter of respect to privacy 
@@ -14,8 +21,26 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 # to interact with users that will be able to transform their images into psychedelic ones. 
 
 # Creating instance of the class
-app=Flask(__name__)
+app = Flask(__name__)
 app.config['SECRET_KEY'] = '592716490af3ccea41cbc1e68e1fc7e3d6f0656d3990af9f'
+app.config['UPLOADED_PHOTOS_DEST'] = UPLOAD_FOLDER
+
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+
+class UploadForm(FlaskForm):
+    photo = FileField(
+        validators=[
+            FileAllowed(photos, 'Only Images are allowed'),
+            FileRequired('File field should not be empty')
+        ]
+    )
+
+    submit = SubmitField('Upload')
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 #to tell flask what url shoud trigger the function index()
@@ -35,19 +60,28 @@ def index():
     
     return render_template("index.html")
 
+@app.route('/uploads/<filename>')
+def get_file(filename):
+    return send_from_directory(app.config['UPLOADED_PHOTOS_DEST'], filename)
+
 @app.route('/create/', methods=['GET', 'POST'])
 def create():
-    if request.method == 'POST':
         # Here we get into interesting things
         # we process the images through our model
         # receive the image
         # format it in the way the model wants
         # inference
         # pass it back to create.html
-        return render_template('create.html')
-        
-    else:
-        return render_template('create.html')
+
+        form = UploadForm()
+        if form.validate_on_submit():
+            filename = photos.save(form.photo.data)
+            file_url =  url_for('get_file', filename=filename)
+        else:
+            file_url = None
+
+        return render_template('create.html', form=form, file_url=file_url)
+   
 
 
 @app.route('/about')
@@ -62,5 +96,5 @@ def contact():
 
 '''
 if __name__ == "__main__":
-app.run(debug=True)
+    app.run(debug=True)
 '''
