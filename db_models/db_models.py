b91003f1cb096d6263bc4e81c1b41e4ef9a02ece
Luis <luigimonge16@gmail.com>
2023-02-04 02:18:36 +0100

    Add tablenames

--- db_models/db_models.py
+++ db_models/db_models.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Upload(db.Model):
+    __tablename__ = 'upload'
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(50))
     data = db.Column(db.LargeBinary)
@@ -11,8 +12,10 @@ class Upload(db.Model):
 
 
 class Generate(db.Model):
+    __tablename__ = 'generate'
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(50))
     data = db.Column(db.LargeBinary)
     mimetype = db.Column(db.String(20))
+    upload_id = db.Column(db.Integer, db.ForeignKey('upload.id'), nullable=False)
     created_on = db.Column(db.DateTime, server_default=db.func.now())
